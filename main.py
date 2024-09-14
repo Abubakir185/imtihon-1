f8e2899bae43d57caff4f49a4a94b24612bba344
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2024-09-14 16:15:21 +0500

    done

---
+++ main.py
@@ -0,0 +1,108 @@
+
+# Imtihon qoidalari, aldamang, atrofga qaramang!
+# Savollarga javob yozing va na'muna kodini taqdim eting
+
+
+# 1-savol: print() bu...
+
+#Javob:
+    # foydalanuvchiga ma'lumotni  korsatish uchun ishlatiladi.
+# Kod namunasi:
+    # print("salom")
+
+
+# 2-savol: input() bu...
+
+#Javob:
+    # foydalanuvchidan ma'lumot olish uchun ishlatiladi.
+# Kod namunasi:
+    # input("Ismingizni kiriting: ")
+
+# 3-savol: O'zgaruvchi nima?
+
+#Javob: 
+    # biror bir qiymatni ozida saqlab turadi.
+# Kod namunasi:
+    # son = 0
+
+
+# 4-savol: 3 ta maʼlumot turini sanab oʻting va misol keltiring:
+
+#Javob:
+    # String, Number, Boolean.
+# Kod namunasi:
+    # string(9)
+    # int(34.8)
+    # bool(0)
+
+# 5-savol: Format string nima (f"")?
+
+#Javob:
+    # ma'lumotni formatlab beradi.
+# Kod namunasi:
+    # f"something {ozgaruvchi} something"
+
+# 6-savol: Mantiqiy operatorlar nima?
+
+#Javob:
+    # Mantiqiy operatorlar or, and, not. 1 tadan kop qiymat yozaolish va ularni tekshirish uchun ishlatiladi.
+# Kod namunasi:
+    #  True or False
+    #  True and False
+    #  True or True
+    #  True and True
+    #  False or False
+    #  False and False
+
+# 7-savol: Taqqoslash operatorlarini sanab bering va ularga misol keltiring:
+
+#Javob:
+    # bir Sonni ikkinchisidan kichik, katta yoki teng ekanligini tekshiradi.
+# Kod namunasi:
+    # son < 2
+    # son > 2
+    # son = 2
+    # son <= 2
+    # son >= 2
+    
+
+# 8-savol: If, elif, else nima va ular bilan misol ko'ring:
+
+#Javob:
+    # tog'ri yoki yoqligini tekshiradi.
+# Kod namunasi:
+    # if son == 4:
+    # print("True")
+    # elif son < 4
+    # print("son kichik")
+    # else:
+#  print(" katta")
+
+# 9-savol: For nima:
+
+#Javob: 
+    # For tsikli son yoki stringda yurip chiqadi.
+# Kod namunasi:
+    # for i in text
+    # for i in range(son)
+
+# 9-savol: While nima:
+
+#Javob: 
+    # shart togri bolmaguncha kodni aylantiradi.
+# Kod namunasi:
+    # while son != 0
+    # input("son:")
+    # print("notog'ri")
+
+# 10-savol: For va while o'rtasidagi farq nima?
+
+#Javob:
+    # for sozni yurip chiqadi while kodni.
+# Kod namunasi:
+    # for i in text:
+    # while shart:
+    
+
+
+# 11-chi: Endi siz  tosh, qog'oz, qaychi o'yinini yaratishingiz kerak.
